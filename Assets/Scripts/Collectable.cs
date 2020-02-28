1fd0d88c970c4a8c0dff75a77bf656b526f03f5b
Jethro Schoppenhorst <38409262+JSchoppe@users.noreply.github.com>
2020-02-28 17:33:32 -0600

    Started Collectable Behavior
    Needs to be finished

---
+++ Assets/Scripts/Collectable.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Collectable : MonoBehaviour
+{
+    private float spinRate = 90;
+
+    private float pickupDuration = 2;
+    private float pickupStartTime;
+    private float pickupArcHeight = 2;
+    private Vector3 startPosition;
+
+    void Update()
+    {
+        transform.eulerAngles += Vector3.up * spinRate * Time.deltaTime;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Player")
+        {
+            gameObject.GetComponent<SphereCollider>().enabled = false;
+            pickupStartTime = Time.time;
+            startPosition = transform.position;
+            StartCoroutine(ObtainCollectable());
+        }
+    }
+
+    private IEnumerator ObtainCollectable()
+    {
+        float interpolant = (Time.time - pickupStartTime) / pickupDuration;
+
+        if(interpolant < 1)
+        {
+
+        }
+        else
+        {
+
+        }
+
+        yield return new WaitForEndOfFrame();
+    }
+}
