6dab0d2da3b875539a43136dd5f8648637570e5b
Jethro Schoppenhorst <38409262+JSchoppe@users.noreply.github.com>
2020-02-28 15:04:11 -0600

    Implemented Knockback

---
+++ Assets/Scripts/KnockbackObstacle.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KnockbackObstacle : MonoBehaviour
+{
+    [SerializeField] private Vector3 knockbackVelocity;
+    [SerializeField] private float knockbackDuration;
+
+    private void OnTriggerEnter(Collider collider)
+    {
+        if(collider.tag == "Player")
+        {
+            ApplyKnockback(
+                collider.gameObject.GetComponent<PlayerMovement>(),
+                collider.attachedRigidbody);
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.collider.tag == "Player")
+        {
+            ApplyKnockback(
+                collision.rigidbody.gameObject.GetComponent<PlayerMovement>(),
+                collision.rigidbody);
+        }
+    }
+
+    private void ApplyKnockback(PlayerMovement player, Rigidbody body)
+    {
+        player.Stun(knockbackDuration);
+        body.AddForce(knockbackVelocity, ForceMode.Impulse);
+    }
+}
