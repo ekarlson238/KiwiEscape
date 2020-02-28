8e27880b118ccba8e2fcc6dc69e07b075f4f8933
Jethro Schoppenhorst <38409262+JSchoppe@users.noreply.github.com>
2020-02-28 16:22:38 -0600

    Audio Singleton
    Prefab created for audio, attach to camera

---
+++ Assets/Scripts/AudioManager.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum BackgroundTrack
+{
+    Menu,
+    Gameplay
+}
+public enum SoundEffect
+{
+    KiwiHurt,
+    KiwiJump
+}
+
+public sealed class AudioManager : MonoBehaviour
+{
+    private static AudioManager self;
+
+    [SerializeField] private AudioSource BGMSource;
+    [SerializeField] private AudioSource SFXSource;
+
+    [SerializeField] private AudioClip menuBGM;
+    [SerializeField] private AudioClip gameplayBGM;
+
+    [SerializeField] private AudioClip[] kiwiHurt;
+    [SerializeField] private AudioClip[] kiwiJump;
+
+    private Dictionary<BackgroundTrack, AudioClip> BGM;
+    private Dictionary<SoundEffect, AudioClip[]> SFX;
+
+    private void Start()
+    {
+        self = this;
+        BGMSource.loop = true;
+        SFXSource.loop = false;
+        BGM[BackgroundTrack.Menu] = menuBGM;
+        BGM[BackgroundTrack.Gameplay] = gameplayBGM;
+        SFX[SoundEffect.KiwiHurt] = kiwiHurt;
+        SFX[SoundEffect.KiwiJump] = kiwiJump;
+    }
+
+    public static void SetBackgroundMusic(BackgroundTrack track)
+    {
+        self.BGMSource.clip = self.BGM[track];
+        self.BGMSource.Play();
+    }
+
+    public static void PlaySoundEffect(SoundEffect effect)
+    {
+        self.SFXSource.clip = self.RandomArrayElement(self.SFX[effect]);
+        self.SFXSource.Play();
+    }
+
+    private T RandomArrayElement<T>(T[] array)
+    {
+        if(array.Length == 0) { return default; }
+        int randomIndex = Random.Range(0, array.Length);
+        return array[randomIndex];
+    }
+}
