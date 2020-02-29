02ae5289d79f7b7a8006cb8bb6e607b258f499ac
swarmsoflizards <emma.steinmetz@gmail.com>
2020-02-29 11:47:53 -0600

    Wrote SceneLoader and UIManager scripts

---
+++ Assets/Scripts/SceneLoader.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SceneLoader : MonoBehaviour
+{
+
+
+    public void LoadMenuScene()
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene("StartMenu");
+    }
+
+    //public void LoadGameScene()
+    //{
+    //    UnityEngine.SceneManagement.SceneManager.LoadScene();
+    //}
+
+}
---
+++ Assets/Scripts/UIManager.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIManager : MonoBehaviour
+{
+
+    [SerializeField] Canvas UICanvas;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
