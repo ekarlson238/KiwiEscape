5a970c2202b11c08d3c1ead25be3ea40b7735c33
Jethro Schoppenhorst <38409262+JSchoppe@users.noreply.github.com>
2020-02-28 18:26:38 -0600

    Started Writing Pickup Animation
    Collectable.cs needs to be completed/refactored

--- Assets/Scripts/Collectable.cs
+++ Assets/Scripts/Collectable.cs
@@ -10,6 +10,8 @@ public class Collectable : MonoBehaviour
     private float pickupStartTime;
     private float pickupArcHeight = 2;
     private Vector3 startPosition;
+    private Transform targetPosition;
+    private Vector3 startScale;
 
     void Update()
     {
@@ -23,23 +25,31 @@ public class Collectable : MonoBehaviour
             gameObject.GetComponent<SphereCollider>().enabled = false;
             pickupStartTime = Time.time;
             startPosition = transform.position;
+            startScale = transform.localScale;
+            targetPosition = other.transform;
             StartCoroutine(ObtainCollectable());
         }
     }
 
     private IEnumerator ObtainCollectable()
     {
+        Debug.Log("coroutine");
         float interpolant = (Time.time - pickupStartTime) / pickupDuration;
 
         if(interpolant < 1)
         {
-
+            transform.localScale = Vector3.Lerp(startScale, Vector3.zero, interpolant);
+            transform.position = new Vector3(
+                Mathf.Lerp(startPosition.x, targetPosition.position.x, interpolant),
+                targetPosition.position.y + pickupArcHeight * (1 -Mathf.Pow(2*interpolant - 1,2)),
+                Mathf.Lerp(startPosition.z, targetPosition.position.z, interpolant)
+                );
         }
         else
         {
-
+            Destroy(gameObject);
+            yield return null;
         }
-
         yield return new WaitForEndOfFrame();
     }
 }
