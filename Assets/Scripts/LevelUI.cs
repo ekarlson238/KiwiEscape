e1d5c22a93a2f77777e372729fa6db32132a3433
caseyjarmes <44095299+caseyjarmes@users.noreply.github.com>
2020-03-01 12:57:10 -0600

    Level UI, Winstate
    -Added victory balloon
    -Added score counter
    -Added winstate

---
+++ Assets/Scripts/LevelUI.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class LevelUI : MonoBehaviour
+{
+    public Text score;
+    public Image endingImage;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    void FixedUpdate()
+    {
+        score.text = $"Score - {Collectable.PlayerCollectableCount}";
+    }
+
+    private void OnCollisionEnter()
+    {
+        StartCoroutine(Win());
+    }
+
+    IEnumerator Win()
+    {
+        endingImage.gameObject.SetActive(true);
+        yield return new WaitForSeconds(3f);
+        SceneManager.LoadScene("StartMenu");
+    }
+}
